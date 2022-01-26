beb626667f98378e7082ae47b44f1cf00b86a599
LJKR2020 <loredana.rietveld@gmail.com>
2022-01-26 14:07:11 +0100

    added context order

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div>
-      Begin hier met de opdracht!
+        <h2>Pokemon naam</h2>
+        <img src="/" alt="pokemon-naam"/>
+        <h4>Moves: Aantal moves</h4>
+        <h4>Weigth: Gewicht</h4>
+        <h4>Abilities:</h4>
+        <p>Soorten moves</p>
     </div>
   );
 }
